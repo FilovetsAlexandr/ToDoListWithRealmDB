813db016c05d8061a24de4c62b85bee8874efa0f
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-11-02 09:25:45 +0300

    project created

---
+++ ToDoListWithRealmBD/ToDoListWithRealmBD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListWithRealmBD
+//
+//  Created by Alexandr Filovets on 2.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
